59f884768f7e44d055671727ca56b7e3e0e1ed52
Matias Doyle <hello@matiasdoyle.com>
2012-11-09 18:04:33 +0000

    Added production config.

--- app.js
+++ app.js
@@ -6,24 +6,31 @@ var express = require('express'),
 
 var app = express();
 
-app.configure(function(){
+
+app.configure('development', function () {
+  app.use(express.logger('dev'));
+  app.use(express.errorHandler());
+});
+
+app.configure('production', function () {
+  console.log('running');
+  app.use(express.logger('tiny'));
+  app.use(express.errorHandler());
+})
+
+app.configure(function () {
   app.set('port', process.env.PORT || 3000);
   app.set('views', __dirname + '/views');
   app.set('view engine', 'jade');
   app.use(express.favicon());
-  app.use(express.logger('dev'));
   app.use(express.bodyParser());
   app.use(express.methodOverride());
   app.use(app.router);
   app.use(express.static(path.join(__dirname, 'public')));
 });
 
-app.configure('development', function(){
-  app.use(express.errorHandler());
-});
-
 app.get('/', routes.index);
 
-http.createServer(app).listen(app.get('port'), function(){
+http.createServer(app).listen(app.get('port'), function () {
   console.log("Express server listening on port " + app.get('port'));
 });
