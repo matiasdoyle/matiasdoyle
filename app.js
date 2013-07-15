87ab95ed1f4328e8591b827adfa866ab1f585a27
Matias Doyle <hello@matiasdoyle.com>
2013-07-15 20:07:28 +0200

    Enabling Nodefly when NODEFLY_KEY env is sat

--- app.js
+++ app.js
@@ -1,11 +1,16 @@
+if (process.env.NODEFLY_KEY) {
+  require('nodefly').profile(
+      process.env.NODEFLY_KEY,
+      'matiasdoyle'
+  );
+}
 
-var express = require('express'),
-    routes = require('./routes'),
-    http = require('http'),
-    path = require('path');
+var express = require('express');
+var routes = require('./routes');
+var http = require('http');
+var path = require('path');
 
 var app = express();
-
 
 app.configure('development', function () {
   app.use(express.logger('dev'));
@@ -13,16 +18,12 @@ app.configure('development', function () {
 });
 
 app.configure('production', function () {
-  console.log('running');
   app.use(express.logger('tiny'));
   app.use(express.errorHandler());
 })
 
 app.configure(function () {
   app.set('port', process.env.PORT || 3000);
-  app.set('views', __dirname + '/views');
-  app.set('view engine', 'jade');
-  app.use(express.favicon());
   app.use(express.bodyParser());
   app.use(express.methodOverride());
   app.use(app.router);
