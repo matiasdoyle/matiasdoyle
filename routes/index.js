8a04359a22bfde65c4cc00036e5eac91c468e923
Matias Doyle <hello@matiasdoyle.com>
2013-07-15 20:08:57 +0200

    Rendering markdown content and view to memory on server start

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,21 @@
-var fs = require('fs'),
-    md = require('markdown');
+var fs = require('fs');
+var md = require('markdown');
+var ejs = require('ejs');
+var view = (process.env.NODE_ENV === 'production' ?
+  './views/production.ejs' : './views/development.ejs');
+var rendered;
+
+fs.readFile('./content/about.md', 'utf8', function (err, data) {
+  var about = md.parse(data);
+  ejs.renderFile(view, {
+    title: "Matias Doyle",
+    about: about
+  }, function(err, str) {
+    if (err) throw err;
+    rendered = str;
+  });
+});
 
 exports.index = function (req, res) {
-  fs.readFile('./content/about.md', 'utf8', function (err, data) {
-    var out = md.parse(data);
-
-    res.render('index', {
-      title : 'Matias Doyle',
-      about : out
-    });
-  });
+  res.send(rendered);
 };
